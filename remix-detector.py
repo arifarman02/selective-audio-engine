b4d1732088e73b0ead821c7d3e5a12bb8c554764
arifarman02 <arifarman1820@gmail.com>
2025-04-14 16:40:41 +0100

    Started work on similarity score

--- remix-detector.py
+++ remix-detector.py
@@ -15,4 +15,23 @@ class RemixDetector:
     def save_database(self, path):
         with open(path, 'wb') as f:
             pickle.dump(self.database, f)
-            
+    
+    def extract_features(self, audio_path):
+        y, sr = librosa.load(audio_path)
+        chroma = librosa.feature.chroma_cqt(y=y, sr=sr)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+        onset_env = librosa.onset.onset_strength(y=y, sr=sr)
+        tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
+        fingerprint= {
+            'chroma_mean': np.mean(chroma, axis=1),
+            'chroma_var': np.var(chroma, axis=1),
+            'mfcc_mean': np.mean(mfcc, axis=1),
+            'mfcc_var': np.var(mfcc, axis=1),
+            'tempo': tempo[0]
+        }
+        return fingerprint
+    
+    def add_original_song(self, song_path, song_id):
+        fingerprint = self.extract_features(song_path)
+        self.database[song_id] = fingerprint
+        return fingerprint
